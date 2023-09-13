5ce7034c4b7a9f5ff5559031f21984d8e161d21d
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-09-13 10:27:42 +0200

    Initial commit

---
+++ less1/Dolgozo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace asdasd
+{
+    internal class Dolgozo
+    {
+        private int fizetes;
+        public string nev;
+        public string szakma;
+        private int szuletesiEv;
+
+        public int Fizetes { get => fizetes; set => fizetes = value > 0 ? value : 0; }
+        public int SzuletesiEv { get => szuletesiEv; set => szuletesiEv = value; }
+
+        public Dolgozo(int fizetes, string nev, string szakma, int szuletesiEv) 
+        {
+            this.fizetes = fizetes;
+            this.nev = nev;
+            this.szakma = szakma;
+            this.szuletesiEv = szuletesiEv;
+        }
+
+        public string GetSzakma()
+        {
+            return this.szakma;
+        }
+        public string SetSzakma(string szakma)
+        {
+            return this.szakma = szakma;
+        }
+        public string Kiir()
+        {
+            return($"Név: {this.nev},Születési év: {this.szuletesiEv},Fizetés: {this.fizetes}Ft,Szakmája: {this.szakma}");
+        }
+    }
+}
---
+++ less1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace asdasd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozo> data = new List<Dolgozo>();
+            string[] szakmaT = { "ács", "asztalos", "hegesztő", "festő", "burkolo" };
+            for (int i = 1; i < 21; i++)
+            {
+                string nev = "Dolgozó" + i;
+                int szuletesiEv = new Random().Next(1960, 2002);
+                int fizetes = new Random().Next(200000, 1000000);
+                int rnd = new Random().Next(0, 4);
+                string szakma = szakmaT[rnd];
+                data.Add(new Dolgozo(fizetes, nev, szakma, szuletesiEv));
+            }
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.Kiir());
+            }
+
+            int legtobbetKeres = data.IndexOf(data.MaxBy(x => x.Fizetes)!);
+            Console.WriteLine();
+            Console.WriteLine(data[legtobbetKeres].Kiir());
+            Console.WriteLine();
+            int acsok = 0;
+
+            foreach (var item in data)
+            {
+                if (item.GetSzakma() == "ács")
+                {
+                    acsok++;
+                }
+            }
+
+            Console.WriteLine($"{acsok} db ács dolgozik");
+        }
+    }
+} 

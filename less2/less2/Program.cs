f4adf3bafa87ed1e0a390e6e17854d718bacb48d
Hudi-B <szegedi.bence.csaba@gmail.com>
2023-09-19 12:08:25 +0200

    lesson

---
+++ less2/less2/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace less2
+{
+    class Cheese
+    {
+        protected int price;
+        protected string origin;
+
+        public Cheese(int price)
+        {
+            this.price = price;
+        }
+        public virtual void Origin()
+        {
+            origin = "";
+        }
+
+        public string Originated()
+        { 
+            return origin;
+        }
+    }
+    class Cheddar : Cheese 
+    {
+        protected string usedIn;
+
+        public Cheddar(int price, string usedIn) : base(price)
+        {
+            this.usedIn = usedIn;
+            base.price += 50000;
+        }
+
+        public override void Origin()
+        {
+            origin = "Italy";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cheese a = new Cheese(200000);
+            Console.WriteLine(a.Originated);
+            Cheddar b = new Cheddar(23211, "Salad");
+            Console.WriteLine(b.Originated);
+        }
+    }
+}

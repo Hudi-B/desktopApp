4ef2f1bfe479d5d6b03836157fe7b91c483d39e8
Hudi-B <vanosskanoss@gmail.com>
2024-01-10 13:11:31 +0100

    lesson0109

---
+++ kzug/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace kzug
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
